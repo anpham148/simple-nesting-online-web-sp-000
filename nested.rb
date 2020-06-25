f5aeb7550faeca0e5cfd3f09f7bfe028a7dfe514
An Pham <anpham148@gmail.com>
2020-06-25 13:23:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,10 +62,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-   :languages => ["LISP", "C"]
-}
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
 
 	programmer_hash =
  		{
@@ -87,7 +87,7 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
-
+programmer_hash
 end
 
 def changing_alan
